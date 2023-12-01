d56f9692592e3936b73b1321e59807099ef9478f
Diahann Castellon <143205792+Diahann-c@users.noreply.github.com>
2023-11-30 20:33:02 -0800

    Added Files
    Uploaded raw files, converted csv files, images of database query execution and final script and schema.

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,55 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(4)   NOT NULL,
+    "category" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" VARCHAR(4)   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    "subcategory" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" VARCHAR(4)   NOT NULL,
+    "contact_id" VARCHAR(4)   NOT NULL,
+    "company_name" VARCHAR(35)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" INT   NOT NULL,
+    "pledged" INT   NOT NULL,
+    "outcome" VARCHAR(10)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
+    "launced_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+
+SELECT * FROM "Contacts"
+SELECT * FROM "Category"
+SELECT * From "Subcategory"
+SELECT * FROM "Campaign"
